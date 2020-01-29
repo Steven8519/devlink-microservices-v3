35b776502cc1dbd2cd8d7d9cacb54648025eba9a
stevenmcdonald <engineers2526@gmail.com>
2020-01-29 13:00:10 -0600

    gradle file update
    updated each build.gradle file for each microservice

--- microservices/recruiter-service/src/test/java/info/devlink/core/recruiterservice/RecruiterServiceApplicationTests.java
+++ microservices/recruiter-service/src/test/java/info/devlink/core/recruiterservice/RecruiterServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package info.devlink.core.recruiterservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecruiterServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
